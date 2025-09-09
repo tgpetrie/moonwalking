3836bfb8fdad85e86e1e25fafe36cc81d8fc4f10
tgpetrie <tgpetrie@gmail.com>
2025-09-08 22:42:02 -0600

    functions: keep only canonical [[path]] proxy; remove shadowing routes and _functions/

--- frontend/_functions/server-info.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet() {
-  return new Response(JSON.stringify({
-    debug: 'Function is working',
-    timestamp: Date.now()
-  }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
